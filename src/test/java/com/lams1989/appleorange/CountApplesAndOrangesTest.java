71ee5ddead39a22bc2bc9056d4e92613f0d6cc6f
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-07-07 15:17:46 -0300

    set implementation

--- src/test/java/com/lams1989/appleorange/CountApplesAndOrangesTest.java
+++ src/test/java/com/lams1989/appleorange/CountApplesAndOrangesTest.java
@@ -2,26 +2,35 @@ package com.lams1989.appleorange;
 
 import junit.framework.TestCase;
 
-public class CountApplesAndOrangesTest extends TestCase{
+public class CountApplesAndOrangesTest extends TestCase {
 
 	public void testCount() {
 		int[] apples = { -2, 4 };
 		int[] oranges = { -2, -1, -3 };
 
 		int[] result = CountApplesAndOranges.count(7, 10, 4, 12, apples, oranges);
-		
+
 		assertEquals(1, result[0]);
 		assertEquals(2, result[1]);
 	}
-	
+
 	public void testCount1() {
 		int[] apples = { 4, 5 };
 		int[] oranges = { -2, -1, -3 };
 
 		int[] result = CountApplesAndOranges.count(7, 10, 4, 12, apples, oranges);
-		
+
+		assertEquals(2, result[0]);
+		assertEquals(0, result[1]);
+	}
+
+	public void testCount2() {
+		int[] apples = { -2, 2, 1 };
+		int[] oranges = { 5, -6, };
+
+		int[] result = CountApplesAndOranges.count(7, 10, 4, 12, apples, oranges);
+
 		assertEquals(1, result[0]);
-		assertEquals(2, result[1]);
+		assertEquals(1, result[1]);
 	}
 }
-
